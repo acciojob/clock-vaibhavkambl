5101668722cfa8e1fbed2a7c0901a3c917a06ace
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-01 12:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateClock() {
+      var now = new Date();
+      var date = now.toDateString();
+      var time = now.toLocaleTimeString();
+      document.getElementById('timer').innerHTML = date + ', ' + time;
+    }
+    setInterval(updateClock, 1000);
